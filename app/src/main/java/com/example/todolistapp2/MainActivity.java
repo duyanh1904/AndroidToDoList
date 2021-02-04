ceb6b5338134d9f11ee8d627d2040c750cb2345f
MocUyenThanh <duyanh3897@gmail>
2021-02-04 18:49:52 +0700

    new

---
+++ app/src/main/java/com/example/todolistapp2/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.todolistapp2;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> toDoList;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toDoList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.list_view_layout,toDoList);
+        listView = findViewById(R.id.id_list_view);
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                TextView textView = (TextView) view;
+                textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            }
+        });
+
+        editText = findViewById(R.id.id_edit_text);
+    }
+
+
+    public void addItemToList(View view) {
+        toDoList.add(editText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+
+        editText.setText("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
